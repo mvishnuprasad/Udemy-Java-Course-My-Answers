e6f2d28c7f60f86687196d9491e201a88273e251
Vishnu Prasad M <vpm735@gmail.com>
2020-10-04 17:33:28 +0530

    Create TeenNumberChecker.java

---
+++ TeenNumberChecker.java
@@ -0,0 +1,21 @@
+  public class TeenNumberChecker {
+        public static boolean hasTeen(int a, int b, int c){
+            if(a<=19&&a>=13||b<=19&&b>=13||c<=19&&c>=13){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        
+        
+        public static boolean isTeen(int d){
+            if(d<=19&&d>=13){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
